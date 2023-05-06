4778cf7e0ed8cd92d71a65943506ea51e2bc0ffe
milka <119007476+milkafasikag@users.noreply.github.com>
2023-05-07 01:02:25 +0300

    Add files via upload

---
+++ lib/timermodel.dart
@@ -0,0 +1,13 @@
+ import 'package:percent_indicator/percent_indicator.dart';
+ import 'timer.dart';
+ import 'dart:async';
+ import 'package:flutter/material.dart';
+ import 'setting.dart';
+
+
+class TimerModel {
+  String time;
+  double percent;
+
+  TimerModel(this.time, this.percent);
+}
